2e0cfb04089bfd8606fc96b1f0a6906414bed5c4
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-09-24 20:48:06 +0600

    add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("levox is runnisng sow")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte(`{"message":"all ok"}`))
+	})
+
+	srv := http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	log.Println("server is running on port", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("server not running on :8080 err: %v", err)
+	}
 }
